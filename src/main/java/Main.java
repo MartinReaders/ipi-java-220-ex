5dbd3534d096432100cf186e200c031181305787
Martin <martin.khapherian@gmail.com>
2020-10-07 16:16:48 +0200

    ex 101

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import com.ipiecoles.java.java220.Entreprise;
+
 public class Main {
     public static void main(String[] args)
     {
 
-        System.out.println(Math.max(4, 10));
+        System.out.println(Entreprise.primeAnnuelleBase());
 
     }
 }
