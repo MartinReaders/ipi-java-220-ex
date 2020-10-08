9955a26deef0a8292ac3bae9c38ec4f1ffd59a95
Martin <martin.khapherian@gmail.com>
2020-10-08 14:56:24 +0200

    Creation get et set Technicien

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -1,4 +1,13 @@
 package com.ipiecoles.java.java220;
 
 public class Technicien extends Employe {
+    private Integer grade;
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
 }
