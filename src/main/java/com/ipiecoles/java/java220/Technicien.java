f27e634946368740e7b414ebc1e3749605b0b1bf
Martin <martin.khapherian@gmail.com>
2020-10-08 15:18:52 +0200

    getPrimeAnnuelle

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -1,7 +1,19 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 public class Technicien extends Employe {
     private Integer grade;
+
+
+    public Technicien(){
+        super();
+    }
+
+    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer grade) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.grade = grade;
+    }
 
     public Integer getGrade() {
         return grade;
@@ -9,5 +21,23 @@ public class Technicien extends Employe {
 
     public void setGrade(Integer grade) {
         this.grade = grade;
+    }
+
+
+    @Override
+    public void setSalaire(Double salaire) {
+        super.setSalaire(salaire * (1+ grade / 10d));
+    }
+
+    @Override
+    public Integer getNbConges() {
+        return super.getNbConges() + this.getNombreAnneeAnciennete();
+    }
+
+    @Override
+    public Double getPrimeAnnuelle() {
+
+
+        return super.getPrimeAnnuelle() *(1+ (grade/10d)) + Entreprise.PRIME_ANCIENNETE * this.getNombreAnneeAnciennete();
     }
 }
